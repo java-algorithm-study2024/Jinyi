2a14499eb86a8f00a13b1cecb5a19157c724b94c
genie-Ryu <69509924+genie-Ryu@users.noreply.github.com>
2024-06-02 17:55:24 +0900

    2024.06.02

---
+++ Stack/Baek10773zero.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int k = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<>();
+		for(int i = 0; i < k; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if(num != 0) stack.push(num);
+			else stack.pop();
+		}
+		int sum = stack.stream().mapToInt(Integer::intValue).sum();
+//		int sum = 0;
+//		while(!stack.isEmpty()) {
+//			sum += stack.pop();
+//		}
+		System.out.println(sum);
+	}
+}
