9390939a00437a4119a68f6a91c02e9524341a4d
genie-Ryu <69509924+genie-Ryu@users.noreply.github.com>
2024-06-20 14:21:13 +0900

    2024.06.20.목

---
+++ DFS/(공통)Pro_타겟넘버.java
@@ -0,0 +1,22 @@
+class Solution {
+    static int answer = 0;
+	
+	public int solution(int[] numbers, int target) {
+		dfs(numbers, 0, target, 0);
+		return answer;
+	}
+	
+	static void dfs(int[] numbers, int depth, int target, int sum) {
+		if(depth == numbers.length) {
+			if(target == sum) {
+				answer++;
+				System.out.println(answer);
+			}
+			return;
+		} else {
+			dfs(numbers, depth + 1, target, sum + numbers[depth]);
+			dfs(numbers, depth + 1, target, sum - numbers[depth]);
+		}
+		
+	}
+}
