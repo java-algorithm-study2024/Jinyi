536acbeecf9ba6988df9483094a66a00a77e0428
Jinyi Ryu <69509924+genie-Ryu@users.noreply.github.com>
2024-09-02 15:21:14 +0900

    2024.08.31.금

---
+++ Implementation/Pro_푸드파이트대회.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        for(int i = 1; i < food.length; i++) {
+            if(food[i] < 2) continue;
+            String str = (i + "").repeat(food[i]/2);
+            answer += str;
+        }
+        StringBuilder sb = new StringBuilder(answer);
+        String rev = sb.reverse().toString();
+        answer += "0";
+        answer += rev;
+        return answer;
+    }
+}
