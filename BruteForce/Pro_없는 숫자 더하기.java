8f2fb08de7539607ac0b86e268aa6c2802cf5b7e
Jinyi Ryu <69509924+genie-Ryu@users.noreply.github.com>
2024-12-01 17:55:05 +0900

    2024.12.01

---
+++ BruteForce/Pro_없는 숫자 더하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 0;
+        for(int i : numbers) {
+            sum += i;
+        }
+        return 45 - sum;
+    }
+}
